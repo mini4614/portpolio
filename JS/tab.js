0dfbf640be2485fdfa5a492c391bc864273c724a
mini4614 <mini4614@gmail.com>
2020-10-31 18:00:11 +0900

    에니메이션 추가

--- JS/tab.js
+++ JS/tab.js
@@ -1,14 +1,13 @@
 const tab = document.querySelector(".js-tab"),
-tab2 = document.querySelector(".js-tab2"),
 close = document.querySelector(".js-close"),
-close2 = document.querySelector(".js-close2"),
-subTitle = tab.querySelector(".subtitle"),
-
-projects = document.querySelector(".projects"),
-
-
 article = document.querySelector(".js-article"),
+
+tab2 = document.querySelector(".js-tab2"),
+close2 = document.querySelector(".js-close2"),
 article2 = document.querySelector(".js-article2"),
+
+subTitle = tab.querySelector(".subtitle"),
+projects = document.querySelector(".projects"),
 panel = document.querySelector(".js-panel");
 
 const SHOWING_TAB = "showt";
@@ -16,22 +15,19 @@ const SHOWING_TAB = "showt";
 const BLUR = "showp";
 
 function handleClick(event)  {
-
          tab.classList.add(SHOWING_TAB);
          panel.classList.add(BLUR);
     }
-
-function handle2Click(event)  {
-
-    tab2.classList.add(SHOWING_TAB);
-    panel.classList.add(BLUR);
-}
-
 function handleCClick(event)  {
     tab.classList.remove(SHOWING_TAB);
     panel.classList.remove(BLUR);
 }
 
+
+function handle2Click(event)  {
+    tab2.classList.add(SHOWING_TAB);
+    panel.classList.add(BLUR);
+}
 function handleC2Click(event)  {
     tab2.classList.remove(SHOWING_TAB);
     panel.classList.remove(BLUR);
